5f001cd8cd6194ff49f4392e3facccd23e666010
estevoliveira <estevoliveira1@gmail.com>
2026-08-09 19:28:59 -0300

    restructuring IAC project

---
+++ backend/s3.tf
@@ -0,0 +1,26 @@
+# Bucket S3 para armazenar o state remoto (compartilhado)
+resource "aws_s3_bucket" "state" {
+  bucket        = "challenge3-terraform-state"
+  force_destroy = true
+  tags = {
+    team = "fiap"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
