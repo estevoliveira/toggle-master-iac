0896de17da0126df3714cd6182716bc9b98984e5
estevoliveira <estevoliveira1@gmail.com>
2026-09-11 22:53:05 -0300

    ajustes finnos

--- modules/root-iam/main.tf
+++ modules/root-iam/main.tf
@@ -98,3 +98,8 @@ resource "aws_iam_role_policy_attachment" "github_actions_terraform" {
   role       = aws_iam_role.github_actions_terraform.name
   policy_arn = aws_iam_policy.github_actions_terraform.arn
 }
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions_terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
